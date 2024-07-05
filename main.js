f7803185f13cb7868c3ea1a8154e1fcead882952
Hotsuen <429007hotsu@gmail.com>
2024-07-06 00:06:56 +0900

    修正

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function number_word(){
+function number_word(num){
     const ones = ["","one", "two","three","four","five","six","seven","eight","nine"];
     const teens = ["", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
     const tens = ["", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
