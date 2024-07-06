ca434f6ad20356c70ad06bd48f598ef2781361fe
Hotsuen <429007hotsu@gmail.com>
2024-07-06 16:35:23 +0900

    修正

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function number_word(num){
 
     let words = "";
 
-    function helper(num, index){
+    function under_thousand(num, index){
         if (num === 0) return "";
 
         if (num < 10){
@@ -18,14 +18,14 @@ function number_word(num){
         }else if (num < 100){
             return tens[Math.floor(num /10)] + " " + ones[num % 10] + " ";
         }else {
-            return ones[Math.floor(num / 100)] + " hundred " + helper(num % 100, 0);
+            return ones[Math.floor(num / 100)] + " hundred " + under_thousand(num % 100, 0);
         }
     }
     let i = 0;
     while (num > 0){
         let tempNum = num % 1000;
         if (tempNum !== 0){
-            words = helper(tempNum, i) + thousands[i] + " " + words;
+            words = under_thousand(tempNum, i) + thousands[i] + " " + words;
         }
         num = Math.floor(num / 1000);
         i++;
